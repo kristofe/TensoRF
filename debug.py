1756b956b3fd13561ffa1ca5b29ac94e7be78259
Kristofer <kristofe72@gmail.com>
2023-01-11 22:18:13 -0500

    Got rendering of density masked color in debugging

--- debug.py
+++ debug.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     density_data =  data['density']
     mask = density_data.copy()
 
-    threshold = 0.05
+    threshold = 0.075
     mask[mask >= threshold] = 1
     mask[mask <  threshold] = 0
     sample_points = torch.stack(torch.meshgrid(
@@ -28,23 +28,10 @@ if __name__ == '__main__':
     ), -1)
     sample_points = -1.5 * (1-sample_points) + 1.5 * sample_points
 
-    sample_points = sample_points[mask>0.5]
-    density_data = density_data[mask>0.5]
-    color_data = color_data[mask>0.5]
-
-
-    #interval = 8
-    #density_data = density_data[::interval,::interval,::interval]
-    #color_data = color_data[::interval,::interval,::interval]
-    #halfz = color_data.shape[2]//2
-    #color = color_data[:,:,halfz,:]
-    #density = density_data[:,:,halfz]
-
-    #color_image = to_image(color, False)
-    #color_image.show()
-    #density_image = to_image(density, False)
-    #density_image.show()
-
+    ids = mask>0.5
+    sample_points = sample_points[ids]
+    density_data = density_data[ids]
+    color_data = color_data[ids]
 
     plt.figure()
     ax = plt.axes(projection="3d")
@@ -56,18 +43,15 @@ if __name__ == '__main__':
     ys = sample_points[:,1].cpu().numpy()
     zs = sample_points[:,2].cpu().numpy()
 
-    #results = grid.sample_nearest(values, sample_points)
-    density_data = torch.from_numpy(density_data)
-    r = density_data.reshape(-1).cpu().numpy()
-    #r = color_data
-    #a = torch.clamp(density_data.reshape(-1).cpu()*8.0, 0.0, 1.0).numpy()
-
+    color_data = torch.from_numpy(color_data)
+    r = color_data.clamp(0.0,1.0).cpu().numpy()
     sc = ax.scatter(xs,ys,zs,c=r, marker='.', cmap='Spectral')
     plt.colorbar(sc)
     plt.show()
 
-    #data = np.load('./test_ml_data.npz')
 
+    '''
+    #data = np.load('./test_ml_data.npz')
     density_lines =  data['density_lines']
     app_lines = data['app_lines']
     dl = density_lines.squeeze(-1).squeeze(1).transpose(1,2,0)
@@ -76,3 +60,4 @@ if __name__ == '__main__':
     al = app_lines.squeeze(-1).squeeze(1).transpose(1,2,0)
     app_image = to_image(al, True)
     app_image.show()
+    '''
